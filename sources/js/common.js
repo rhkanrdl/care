d944d8ae1a80474274af6135447520047a8b517d
rhkanrdl <emrdl7@gmail.com>
2020-02-19 17:02:44 +0900

    접근성 심사 대비

--- sources/js/common.js
+++ sources/js/common.js
@@ -182,29 +182,5 @@ try {
 } catch(e) {}
 
 
-     // map
-
-     var container = document.getElementById('map'); 
-     var options = {
-        center: new kakao.maps.LatLng(33.496309, 126.532741), 
-        level: 3 
-     };
-     
-     var map = new kakao.maps.Map(container, options);
-
-     map.setDraggable(true);
-
-     map.setZoomable(true);
-     
-     // 마커가 표시될 위치입니다 
-     var markerPosition  = new kakao.maps.LatLng(33.496309, 126.532741);
-     
-     // 마커를 생성합니다
-     var marker = new kakao.maps.Marker({
-         position: markerPosition
-     });
-     
-     // 마커가 지도 위에 표시되도록 설정합니다
-     marker.setMap(map);
 
 });
